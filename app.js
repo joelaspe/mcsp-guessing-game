e2af54eeb10134ff929ba928011cbbeafc53ebe1
joelaspe <joe.laspe@gmail.com>
2023-05-05 13:50:49 -0700

    Completed feature 7, and a working scoreboard.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var name = prompt("What is your name?");
+var scoreBoard = {
+    /* 
+    `Chief`: 5
+    `Joey`: 7 */
+}
+var name = prompt("Welcome to the guessing game. What is your name?");
 
-guessingGame(name);
+
+guessingGame(name, scoreBoard);
 
 function playAgain() {
     var result = prompt("Play again? (yes/no)");
@@ -11,25 +17,51 @@ function playAgain() {
     return false;
 }
 
-function guessingGame(name) {
-    var input = prompt(`Hello ${name}! Guess a number`);
-    var num = getRandomInt(1,100);
+function guessingGame(name, scoreBoard) {
+    var input = parseInt(prompt(`Hello ${name}! Guess a number between 1 and 100`));
+    //var num = getRandomInt(1,100);
+    num = 50;
     var arrayTries = [input];
-    console.log(input);
-    while(input != num)
+    while(input !== num)
     {
         if(input > num ) {
-            alert(`Lower ${name}...`);
+            input = parseInt(prompt((`Lower ${name}...`)));
         }
         else {
-            alert(`Higher ${name}...`);
+            input = parseInt(prompt((`Higher ${name}...`)));
         }
-        input = prompt(`Guess again`);
         arrayTries.push(input);
     }
+    // the user guessed the correct number, display the high scores and the last games result
     alert(`Correct ${name}! Your previous guesses were ` + arrayTries.join(`, `) + `.`);
+    var numGuess = arrayTries.length;
+    
+    if(scoreBoard[name] === undefined)
+    {
+        scoreBoard[name] = numGuess;
+    } else if(numGuess < scoreBoard[name])
+    {
+        scoreBoard[name] = numGuess;
+    }
+    var scoreBoardEntries = Object.entries(scoreBoard); // [['Nathan', 5], ['Joey', 3]]
+    console.log(scoreBoardEntries);
+    var scoreBoardString = ''; 
+    var emptySpace = ' ';
+    for(let i = 0; i < scoreBoardEntries.length; i++)
+    {
+        var whitespaceSize = 10 - scoreBoardEntries[i][0].length; //determine whitespace to add
+        //scoreBoardString += scoreBoardEntries[i].join(':'); //--> 'Nathan     :     5'
+       scoreBoardString += scoreBoardEntries[i][0] + emptySpace.repeat(whitespaceSize) + ':     ' + scoreBoardEntries[i][1] + '\n';
+      
+    }
+    console.log(scoreBoardString);
+    alert(scoreBoardString);
     if(playAgain()) {
-        guessingGame(name);
+        guessingGame(name, scoreBoard);
+    }
+    else{
+        name = prompt("What is your name?");
+        guessingGame(name, scoreBoard);
     }
 }
 
@@ -39,3 +71,14 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
   
+
+/*
+
+  Joey: 5
+  Nathan: 6
+  Chief: 10
+
+  Joey     :    5
+  Nathan   :    6
+  Chief    :    10
+  */
